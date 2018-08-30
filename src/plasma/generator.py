a04d9784d8588a34b0590c0928cf240bd1626189
leo <basov.leo@googlemail.com>
2018-08-30 11:02:53 +0200

    Extend module generator.

--- src/plasma/generator.py
+++ src/plasma/generator.py
@@ -2,7 +2,13 @@ from plasma import particle
 from geometry import domain
 import random
 
-def generate_particles(number,values):
+class Values:
+	def __init__(self,weight,mass,temperature):
+		self.weight = weight
+		self.mass = mass
+		self.temperature = temperature
+
+def generate_particles(number,values,domain):
 	particles = []
 
 	for i in range(number):
@@ -10,6 +16,10 @@ def generate_particles(number,values):
 
 		part.weight = values.weight
 		part.mass = values.mass
+
+		part.position[0] = random.uniform(domain.xmin,domain.xmax)
+		part.position[1] = random.uniform(domain.ymin,domain.ymax)
+		part.position[2] = random.uniform(domain.zmin,domain.zmax)
 
 		particles.append(part)
 
