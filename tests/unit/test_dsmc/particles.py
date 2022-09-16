645b46d1fb79e953b54477a30b8c8c7d66408905
Leo Basov <l.basov@sparc-industries.com>
2022-09-16 12:02:35 +0200

    extended unit tests

--- tests/unit/test_dsmc/particles.py
+++ tests/unit/test_dsmc/particles.py
@@ -5,3 +5,13 @@ class TestParticles(unittest.TestCase):
     def test_box_muller(self):
         T = 300
         result = pa.box_muller(T)
+
+    def test_x2velocity(self):
+        x = 1.0
+        mass = 1.0e-26
+        result = pa.x2velocity(x, mass)
+
+    def test_get_vel(self):
+        T = 300
+        mass = 1.0e-26
+        result = pa.get_vel(T, mass)
