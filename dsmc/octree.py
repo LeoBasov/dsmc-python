d6919a48c06329756bfd70bc3a1afdfc696d309d
Leo Basov <leo.basov@dlr.de>
2022-09-21 16:00:02 +0200

    added typing to octree

--- dsmc/octree.py
+++ dsmc/octree.py
@@ -1,13 +1,14 @@
 import math
 import numpy as np
+import numpy.typing as npt
 from numba import njit
 
 fmin = np.finfo(float).min
 fmax = np.finfo(float).max
 
 @njit
-def _find_bounding_box(positions):
-    box = np.array([[fmax, fmin], [fmax, fmin], [fmax, fmin]])
+def _find_bounding_box(positions : npt.NDArray) -> npt.NDArray:
+    box = np.array([[fmax, fmin], [fmax, fmin], [fmax, fmin]], dtype=float)
     
     for pos in positions:
         for i in range(3):
@@ -19,7 +20,7 @@ def _find_bounding_box(positions):
     return box
 
 @njit    
-def _calc_N_res(w, sigma_T, n):
+def _calc_N_res(w : float, sigma_T : float, n : float) -> int:
     """
     Parameters
     ----------
@@ -34,7 +35,7 @@ def _calc_N_res(w, sigma_T, n):
     return int(round(np.sqrt(2.0) / (32.0 * w * sigma_T**3 * n**2)))
 
 @njit  
-def _calc_n(box, N, w):
+def _calc_n(box : npt.NDArray, N : float, w : float) -> float:
     """Calculates number density in cell
     
     Parameters
@@ -53,7 +54,7 @@ def _calc_n(box, N, w):
     return np.prod(np.array([box[i][1] - box[i][0] for i in range(3)])) * N / w
 
 @njit  
-def _is_resolved(box, N, w, sigma_T, Nmin, Nmax):
+def _is_resolved(box : npt.NDArray, N : int, w : float, sigma_T : float, Nmin : int, Nmax : int) -> bool:
     n = _calc_n(box, N, w)
     N = _calc_N_res(w, sigma_T, n)
     
@@ -93,7 +94,7 @@ class Octree:
         self.cell_bounding_boxes = []
         self.cell_offsets = []
         
-    def build(self, positions):
+    def build(self, positions : npt.NDArray):
         bounding_box = _find_bounding_box(positions)
         self.permutations = np.array([i for i in range(len(positions))])
         self._insert_first_leaf(positions, bounding_box)
@@ -105,7 +106,7 @@ class Octree:
     def build_level(self):
         pass
         
-    def _insert_first_leaf(self, positions, box):
+    def _insert_first_leaf(self, positions : npt.NDArray, box : npt.NDArray):
         level = 0;
         elem_offset = 0;
         number_elements = len(positions)
