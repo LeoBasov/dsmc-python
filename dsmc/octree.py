8aba1a2a5ca4e4290c3fd8de1c2e9bc50844eb2a
Leo Basov <leo.basov@dlr.de>
2022-09-21 14:30:19 +0200

    implemented _is_resolved

--- dsmc/octree.py
+++ dsmc/octree.py
@@ -35,7 +35,8 @@ def _calc_N_res(w, sigma_T, n):
 
 @njit  
 def _calc_n(box, N, w):
-    """
+    """Calculates number density in cell
+    
     Parameters
     ----------
     box : np.array(3, 3)
@@ -50,15 +51,26 @@ def _calc_n(box, N, w):
     number density : float
     """
     return np.prod(np.array([box[i][1] - box[i][0] for i in range(3)])) * N / w
+
+@njit  
+def _is_resolved(box, N, w, sigma_T, Nmin, Nmax):
+    n = _calc_n(box, N, w)
+    N = _calc_N_res(w, sigma_T, n)
+    
+    return N > 2 * min(Nmin, max(Nmin, N))
     
 class Leafs:
     def __init__(self):
+        self.clear()
+        
+    def clear(self):
         self.level = []
         self.elem_offset = []
         self.number_elements = []
         self.id_parent = []
         self.id_first_child = []
         self.number_children = []
+        self.N = 0
         
     def add_leaf(self, level, elem_offset, number_elements, id_parent, id_first_child, number_children):
         self.level.append(level)
@@ -67,6 +79,7 @@ class Leafs:
         self.id_parent.append(id_parent)
         self.id_first_child.append(id_first_child)
         self.number_children.append(number_children)
+        self.N += 1
         
 class Octree:
     def __init__(self):
@@ -74,6 +87,9 @@ class Octree:
         self.permutations = None
         self.sigma_T = 3.631681e-19
         self.max_level = 20
+        self.w = 1e+6
+        self.Nmin = 8
+        self.Nmax = 160
         self.cell_bounding_boxes = []
         self.cell_offsets = []
         
@@ -81,9 +97,13 @@ class Octree:
         bounding_box = _find_bounding_box(positions)
         self.permutations = np.array([i for i in range(len(positions))])
         self._insert_first_leaf(positions, bounding_box)
+        last_leaf = 0
         
         for i in range(self.max_level):
             pass
+            
+    def build_level(self):
+        pass
         
     def _insert_first_leaf(self, positions, box):
         level = 0;
