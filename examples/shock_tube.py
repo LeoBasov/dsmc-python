f490a3fd2a567ed11aa65c0cbb0739688d667443
Leo Basov <l.basov@sparc-industries.com>
2022-09-23 15:03:11 +0200

    updated shock tube test case

--- examples/shock_tube.py
+++ examples/shock_tube.py
@@ -2,26 +2,43 @@ import dsmc
 import dsmc.diagnostics as dia
 import matplotlib.pyplot as plt
 
+def write2file(solver, n_file, T_file, p_file):
+    bins, box = dia.sort_bin(solver.particles.Pos, 2, Nbins)
+    N = [len(b) for b in bins]
+    n = dia.calc_n(bins, box, 2, solver.w)
+    T = dia.calc_T(bins, solver.particles.Vel, mass)
+    p = dia.calc_p(n, T)
+      
+    for i in range(Nbins):
+        n_file.write("{},".format(n[i]))
+        T_file.write("{},".format(T[i]))
+        p_file.write("{},".format(p[i]))
+                
+    n_file.write("\n")
+    T_file.write("\n")
+    p_file.write("\n")
+
 if __name__ == '__main__':
     # general parameters
     solver = dsmc.DSMC()
-    domain = ((-0.1e-3, 0.1e-3), (-0.1e-3, 0.1e-3), (0, 50e-3))
+    domain = [(-0.0001, 0.0001), (-0.0001, 0.0001), (0.0, 0.1)]
     dt = 1e-7
-    w = 2.4134e+7
+    w = 1e+9
     mass = 6.6422e-26
     niter = 300
     Nbins = 100
     
     # low denisty particles
-    nhigh = 2.5e+20
+    nhigh = 2.41432e+22 
     Thigh = 300
-    Boxhigh = ((-0.1e-3, 0.1e-3), (-0.1e-3, 0.1e-3), (25e-3, 50e-3))
+    Boxhigh = [(-0.0001, 0.0001), (-0.0001, 0.0001), (0.05, 0.1)]
     
     # high denisty particles
-    nlow = 2.5e+19
+    nlow = 2.41432e+21
     Tlow = 300
-    Boxlow = ((-0.1e-3, 0.1e-3), (-0.1e-3, 0.1e-3), (0, 25e-3))
+    Boxlow = [(-0.0001, 0.0001), (-0.0001, 0.0001), (0.0, 0.05)]
     
+    # setup solver
     solver.set_domain(domain)
     solver.set_weight(w)
     solver.set_mass(mass)
@@ -29,29 +46,17 @@ if __name__ == '__main__':
     solver.create_particles(Boxlow, Tlow, nlow)
     solver.create_particles(Boxhigh, Thigh, nhigh)
     
+    # open files
     n_file = open("n.csv", "w")
     T_file = open("T.csv", "w")
     p_file = open("p.csv", "w")
     
     for it in range(niter):
         print("iteration {:4}/{}".format(it + 1, niter), end="\r", flush=True)
-        solver.advance(dt)
-            
-        bins, box = dia.sort_bin(solver.particles.Pos, 2, Nbins)
-        N = [len(b) for b in bins]
-        n = dia.calc_n(bins, box, 2, solver.w)
-        T = dia.calc_T(bins, solver.particles.Vel, mass)
-        p = dia.calc_p(n, T)
-      
-        for i in range(Nbins):
-            n_file.write("{},".format(n[i]))
-            T_file.write("{},".format(T[i]))
-            p_file.write("{},".format(p[i]))
-                
-        n_file.write("\n")
-        T_file.write("\n")
-        p_file.write("\n")
+        solver.advance(dt)        
+        write2file(solver, n_file, T_file, p_file)
 
+	# close files
     n_file.close()
     T_file.close()
     p_file.close()        
