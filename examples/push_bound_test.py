251b6e8a05a604c234ac37342147ff6ca1c055e1
Leo Basov <leo.basov@dlr.de>
2022-09-26 09:24:36 +0200

    updated push_bound test case

--- examples/push_bound_test.py
+++ examples/push_bound_test.py
@@ -1,4 +1,32 @@
 import dsmc
+import dsmc.writer as wrt
 
 if __name__ == '__main__':
-	pass # create box and see if particles move and refect properly
+	# general parameters
+    solver = dsmc.DSMC()
+    domain = [(-0.5, 0.5), (-0.5, 0.5), (-0.5, 0.5)]
+    dt = 1e-5
+    w = 1e+17
+    n = 1e18
+    mass = 6.6422e-26
+    T = 300
+    niter = 600
+    
+    # setup solver
+    solver.set_domain(domain)
+    solver.set_weight(w)
+    solver.set_mass(mass)
+    
+    solver.create_particles(domain, T, n)
+    
+    for it in range(niter):
+        print("iteration {:4}/{:4}".format(it + 1, niter), end="\r", flush=True)
+        solver.advance(dt, collisions=False)
+        
+        with open("particles_{}.csv".format(it), "w") as file:
+            file.write("x, y, z\n")
+            for pos in solver.particles.Pos:
+                file.write("{},{},{}\n".format(pos[0], pos[1], pos[2]))
+            
+    wrt.write_buttom_leafs(solver.octree)
+    print("done")
