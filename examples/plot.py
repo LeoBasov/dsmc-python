f85278064cd2b7b035ffa53ce69f3e116ee04065
Leo Basov <leo.basov@dlr.de>
2022-09-29 17:19:00 +0200

    extended plot script

--- examples/plot.py
+++ examples/plot.py
@@ -6,6 +6,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('file_name', type=str)
+    parser.add_argument('-ref', type=str)
 
     args = parser.parse_args()
 
@@ -15,6 +16,19 @@ if __name__ == '__main__':
         for line in reader:
             l = [m for m in line if m]
             n = [float(l[i]) for i in range(len(l))]
-            
-        plt.plot(n)
-        plt.show()
+           
+    plt.plot(np.linspace(0, 0.1, len(n)), n)
+    
+    if args.ref:
+        x = []
+        val = []
+        with open(args.ref) as file:
+            reader = csv.reader(file, delimiter=',')
+        
+            for line in reader:
+                x.append(float(line[0]))
+                val.append(float(line[1]))
+                
+        plt.plot(x, val)
+    
+    plt.show()
