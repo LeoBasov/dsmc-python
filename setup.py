9d2e54cd36b162da1c311e61453cfd7df44db349
Leo Basov <basov.leo@gmail.com>
2022-10-01 10:08:39 +0200

    updated changelog and setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 setup(
     name='dsmc',
-    version='0.6.0',
+    version='0.7.0',
     author='Leo Basov',
-    python_requires='>=3.6, <4',
+    python_requires='>=3.10, <4',
     packages=["dsmc"],
     install_requires=[
         'numpy',
