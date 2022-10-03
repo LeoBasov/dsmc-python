2a01d06b7460031566073182b46dcb8654e7bb1d
Leo Basov <40352679+LeoBasov@users.noreply.github.com>
2022-10-03 20:15:28 +0200

    implemented 2d sort (#18)

--- dsmc/common.py
+++ dsmc/common.py
@@ -1,5 +1,11 @@
-from numba import cfunc
+from numba import cfunc, njit
 
 @cfunc("double(double[::3, ::2])")
 def get_V(a):
     return (a[0][1] - a[0][0]) * (a[1][1] - a[1][0]) * (a[2][1] - a[2][0])
+
+@njit 
+def swap(arr, pos1, pos2):
+    temp = arr[pos2]
+    arr[pos2] = arr[pos1]
+    arr[pos1] = temp
--- dsmc/diagnostics.py
+++ dsmc/diagnostics.py
@@ -1,6 +1,67 @@
 import numpy as np
+from numba import njit
 from . import particles as prt
 from . import octree as oc
+from . import common as com
+
+@njit
+def _in_2d_box(pos, box):
+    a = pos[0] >= box[0][0] and pos[0] <= box[0][1]
+    b = pos[1] >= box[1][0] and pos[1] <= box[1][1]
+    return a and b
+
+@njit
+def _set_up_2d_boxes(x0, x1, y0, y1, Nx, Ny):
+    dx = (x1 - x0) / Nx
+    dy = (y1 - y0) / Ny
+    boxes = np.empty((Nx*Ny, 2, 2))
+    y = y0
+    
+    for i in range(Ny):
+        x = x0
+        for j in range(Nx):
+            k = j + i*Nx
+            boxes[k][0][0] = x
+            boxes[k][0][1] = x + dx
+            boxes[k][1][0] = y
+            boxes[k][1][1] = y + dy
+            x += dx
+        y += dy
+    
+    return boxes
+
+@njit
+def _sort_2d(positions, x0, x1, y0, y1, Nx, Ny):
+    boxes = _set_up_2d_boxes(x0, x1, y0, y1, Nx, Ny) # [] : bix id, [][] : axis, [][][] : 0 min , 1 max
+    permutations = [i for i in range(len(positions))]
+    leafs = np.zeros((Nx * Ny, 2), dtype=np.int_) # leaf[0] : ofseat, leaf[1] : n-parts
+    
+    for i in range(len(leafs)):
+        leafs[i][0] = leafs[i - 1][0] + leafs[i - 1][1] if i > 0 else 0
+        leafs[i][1] = 0
+        runner = leafs[i][0]
+        for j in range(leafs[i][0], len(positions)):
+            p = permutations[j]
+            if _in_2d_box(positions[p], boxes[i]):
+                com.swap(permutations, j, runner)
+                runner += 1
+                leafs[i][1] += 1
+
+    return (permutations, leafs, boxes)
+
+class Values:
+    def __init__(self):
+        self.number_elements = 0
+
+def analyse_2d(positions, x0, x1, y0, y1, Nx, Ny):
+    permutations, leafs, boxes = _sort_2d(positions, x0, x1, y0, y1, Nx, Ny)
+    values = [Values() for _ in range(len(leafs))]
+    
+    for i in range(len(leafs)):
+        values[i].number_elements = leafs[i][1]
+        
+    return (boxes, values)
+                    
 
 def sort_bin(positions, axis, Nbin):
     bins = [[] for _ in range(Nbin)]
