c71a39ac77fecd26d7b4065ffd1d686684a56ef9
Leo Basov <leo.basov@dlr.de>
2022-10-04 10:47:50 +0200

    fixed bug in test case

--- examples/hypersonic_flow.py
+++ examples/hypersonic_flow.py
@@ -10,12 +10,13 @@ if __name__ == '__main__':
     domain = [(-3.0, 3.0), (-1.5, 1.5), (-0.025, 0.025)]
     obj = [(-0.25, 0.25), (-0.25, 0.25), (-0.5, 0.5)]
     dt = 1e-6
-    w = 2.3e+14
+    w = 0.2 * 2.3e+15
     mass = 6.6422e-26
     T =  273.0
     n = 2.6e+19
     u = np.array([0.0, -3043.0, 0.0])
-    niter = 1
+    niter = 2001
+    niter_sample = 10
     
     # set up diagnostics values
     x0 = domain[0][0]
@@ -33,13 +34,13 @@ if __name__ == '__main__':
     # set up boundary
     solver.set_bc_type("xmin", "inflow")
     solver.set_bc_type("xmax", "inflow")
-    solver.set_bc_type("ymin", "inflow")
+    solver.set_bc_type("ymax", "inflow")
     
-    solver.set_bc_type("ymax", "open")
+    solver.set_bc_type("ymin", "open")
     
     solver.set_bc_values("xmin", T, n, u)
     solver.set_bc_values("xmax", T, n, u)
-    solver.set_bc_values("ymin", T, n, u)
+    solver.set_bc_values("ymax", T, n, u)
     
     # set object
     solver.add_object(obj)
@@ -53,8 +54,14 @@ if __name__ == '__main__':
     for it in range(niter):
         print("iteration {:4}/{}".format(it + 1, niter), end="\r", flush=True)
         solver.advance(dt)
+            
+    for it in range(niter_sample):
+        print("iteration {:4}/{}".format(it + 1, niter), end="\r", flush=True)
+        solver.advance(dt)
         boxes, values = dia.analyse_2d(solver.particles.Pos, x0, x1, y0, y1, Nx, Ny)
         wrt.write_planar(boxes, values, "hypersonic_{}.vtu".format(it))
+        
+    wrt.write_buttom_leafs(solver.octree)
         
     print("")
     print("--- %s seconds ---" % (time.time() - start_time))
