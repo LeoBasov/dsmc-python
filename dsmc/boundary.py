470a0767141637a456812be6dbe7bfd4d7052c42
Leo Basov <l.basov@sparc-industries.com>
2022-10-07 11:10:32 +0200

    implemented _intersect

--- dsmc/boundary.py
+++ dsmc/boundary.py
@@ -8,6 +8,7 @@ def _check_if_parallel(v1, v2, diff=1e-6):
     
     return V1.dot(V2) > diff
 
+@njit
 def _intersect(l0, l1, p0, p1, p2):
     """
     Args:
@@ -18,4 +19,9 @@ def _intersect(l0, l1, p0, p1, p2):
         p2 : first position on plane
     """
     n_l = l1 - l0
-    n_p = (p1 - p0).cross(p2 - p1)
+    n_p = np.cross((p1 - p0), (p2 - p1))
+    
+    if _check_if_parallel(n_l, n_p):
+        return (False, n_l, n_p, 0.0)
+    else:
+        return (True, n_l, n_p, - ((l0 - p0).dot(n_p) / n_p.dot(n_l)))
--- tests/unit/test_dsmc/boundary.py
+++ tests/unit/test_dsmc/boundary.py
@@ -12,3 +12,25 @@ class TestCommon(unittest.TestCase):
         self.assertTrue(bo._check_if_parallel(v1, v2))
         self.assertFalse(bo._check_if_parallel(v1, v3))
         self.assertFalse(bo._check_if_parallel(v1, v4))
+        
+    def test__intersect(self):
+        l0 = np.array([-1.0, -1.0, 0.0])
+        l1 = np.array([1.0, 1.0, 0.0])
+        
+        p0 = np.array([0.0, -1.0, -1.0])
+        p1 = np.array([0.0, -1.0, 1.0])
+        p2 = np.array([0.0, 1.0, 1.0])
+        
+        intersected, n_l, n_r, t = bo._intersect(l0, l1, p0, p1, p2)
+        
+        self.assertTrue(intersected)
+        
+        self.assertEqual(2.0, n_l[0])
+        self.assertEqual(2.0, n_l[1])
+        self.assertEqual(0.0, n_l[2])
+        
+        self.assertEqual(-4.0, n_r[0])
+        self.assertEqual(0.0, n_r[1])
+        self.assertEqual(0.0, n_r[2])
+        
+        self.assertEqual(0.5, t)
