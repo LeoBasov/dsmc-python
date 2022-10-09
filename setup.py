58455b2f5d397523a79c22f30cc6a5ca582c8f08
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2022-10-09 20:09:49 +0200

    updated changelog and setup.py version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='dsmc',
-    version='0.8.0',
+    version='0.9.0',
     author='Leo Basov',
     python_requires='>=3.10, <4',
     packages=["dsmc", "dsmc.writer", "dsmc.mesh"],
