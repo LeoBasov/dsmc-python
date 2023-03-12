fcab9872fc3a4a313fa4cc80d94fa3d7b606263c
Leo Basov <basov.leo@gmail.com>
2023-03-12 14:09:26 +0100

    added chashign to common njut functions

--- dsmc/common.py
+++ dsmc/common.py
@@ -5,13 +5,13 @@ import numpy.typing as npt
 def get_V(a):
     return (a[0][1] - a[0][0]) * (a[1][1] - a[1][0]) * (a[2][1] - a[2][0])
 
-@njit 
+@njit(cache=True)
 def swap(arr, pos1, pos2):
     temp = arr[pos2]
     arr[pos2] = arr[pos1]
     arr[pos1] = temp
     
-@njit
+@njit(cache=True)
 def is_inside(position : npt.NDArray, box : npt.NDArray) -> bool:
     a : bool = position[0] >= box[0][0] and position[0] <= box[0][1]
     b : bool = position[1] >= box[1][0] and position[1] <= box[1][1]
